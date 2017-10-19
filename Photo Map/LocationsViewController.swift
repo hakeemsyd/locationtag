dfba279d93d869b2f8c205560440175fc9484901
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-10-18 20:06:22 -0700

    Image picker

--- Photo Map/LocationsViewController.swift
+++ Photo Map/LocationsViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
+protocol LocationViewControllerDelegate : class {
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber)
+}
+
 class LocationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     // TODO: Fill in actual CLIENT_ID and CLIENT_SECRET
-    let CLIENT_ID = "CLIENT_ID GOES HERE"
-    let CLIENT_SECRET = "CLIENT_SECRET GOES HERE"
+    let CLIENT_ID = "QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL"
+    let CLIENT_SECRET = "W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU"
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-
+    weak var delegate: LocationViewControllerDelegate!
     var results: NSArray = []
     
     override func viewDidLoad() {
@@ -53,6 +57,9 @@ class LocationsViewController: UIViewController, UITableViewDelegate, UITableVie
 
         let latString = "\(lat)"
         let lngString = "\(lng)"
+        
+        delegate.locationsPickedLocation(controller: self, latitude: lat, longitude: lng)
+        self.dismiss(animated: true, completion: nil)
 
         print(latString + " " + lngString)
     }
@@ -95,5 +102,4 @@ class LocationsViewController: UIViewController, UITableViewDelegate, UITableVie
         });
         task.resume()
     }
-
 }
--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+    LocationViewControllerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +26,56 @@ class PhotoMapViewController: UIViewController {
     }
     
 
+    @IBAction func cameraButtonPressed(_ sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        //vc.sourceType = UIImagePickerControllerSourceType.camera
+        
+        self.present(vc, animated: true, completion: nil)
+
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true) {
+            //segue to tagSegue
+            
+            self.performSegue(withIdentifier: "tagSegue", sender: self)
+        }
+    }
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if( segue.identifier == "tagSegue" ){
+            let destinationViewController = segue.destination as! LocationsViewController
+            destinationViewController.delegate = self
+        }
     }
     
-
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        print("location picked")
+        let annot = MKPointAnnotation()
+        annot.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        
+        mapView.addAnnotation(annot)
+        //poptoview
+    }
 }
